9471f8e7693e5d1ab341525e20b48218085975cf
emagombe <edson.patricio.39@gmail.com>
2019-11-23 16:04:48 +0200

    Fixed group by and order by problem and added select documentation

--- Examples/Select.php
+++ Examples/Select.php
@@ -24,3 +24,22 @@ print_r($sql);
 
 # Returns
 # SELECT user.*, user_type.* FROM user, user_type WHERE user.id = '1' AND user.id BETWEEN 1 AND 7
+
+print_r("\n");
+
+/* Order by */
+$sql = JQB::Select(array(
+	"columns" => array("*"),
+	"from" => array("user", "user_type"),
+	"where" => array(
+		array(
+			"operator" => "like", # It may be =, !=, <>, >= or <=
+			"columns" => array(
+				"user.id" => "1"
+			)
+		),
+	),
+	"order" => array("by" => "user.id", "order" => "asc"),
+))->sql();
+
+print_r($sql);
